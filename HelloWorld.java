d81e54f07f0708e40ab9bed9f35db2edb74027a7
sonasiapps <sonasiapps@gmail.com>
2022-02-27 19:31:43 +0700

    ccc

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
 	public static void main(String args[]){
 		System.out.println("Hello World!");
-        System.out.println("Test Webhook!1");
+        System.out.println("Test Webhook!2");
 	}
 }
